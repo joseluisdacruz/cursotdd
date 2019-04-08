d2abe5d92e1f9a3ebe163fa4ca7aba8f0ed876e5
17890486804 <jose-luis.cruz@serpro.gov.br>
2019-04-08 15:54:50 -0300

    Initial commit

---
+++ camelcase/src/test/java/camelcase/CamelCaseTest.java
@@ -0,0 +1,14 @@
+package camelcase;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CamelCaseTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
