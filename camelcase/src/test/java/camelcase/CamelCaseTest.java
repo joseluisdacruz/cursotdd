db8a0137d900fbd141207a69a97aad154e2ee4a5
17890486804 <jose-luis.cruz@serpro.gov.br>
2019-04-12 14:05:06 -0300

    primeiro commit

--- camelcase/src/test/java/camelcase/CamelCaseTest.java
+++ camelcase/src/test/java/camelcase/CamelCaseTest.java
@@ -2,13 +2,21 @@ package camelcase;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class CamelCaseTest {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	private CamelCaseSplitter ccs;
+	
+	@Before
+	private void init() {
+		css = new CamelCaseSpritter();
 	}
-
+	
+	@Test
+	public void testStringSemMaiuscula() {
+		String str = "fulano";
+		
+	}
 }
