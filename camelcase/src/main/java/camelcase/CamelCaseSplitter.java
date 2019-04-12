31057a53c27f2dd820eca9710133a1bd1f8c6ced
17890486804 <jose-luis.cruz@serpro.gov.br>
2019-04-12 14:38:04 -0300

    implementação converterCamelCase

---
+++ camelcase/src/main/java/camelcase/CamelCaseSplitter.java
@@ -0,0 +1,14 @@
+package camelcase;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CamelCaseSplitter {
+
+	public static List<String> converterCamelCase(String string) {
+		List<String> retorno = new ArrayList<>();
+		retorno.add(string);
+		return retorno;
+	}
+
+}
