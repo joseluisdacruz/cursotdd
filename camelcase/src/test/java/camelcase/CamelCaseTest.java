1386f6cd35c4e2c29c16ecaa6449dc040cfd8380
17890486804 <jose-luis.cruz@serpro.gov.br>
2019-04-12 14:35:15 -0300

    primeiro teste, agora vai

--- camelcase/src/test/java/camelcase/CamelCaseTest.java
+++ camelcase/src/test/java/camelcase/CamelCaseTest.java
@@ -5,21 +5,13 @@ import static org.junit.Assert.assertEquals;
 import java.util.Arrays;
 import java.util.List;
 
-import org.junit.Before;
 import org.junit.Test;
 
 public class CamelCaseTest {
-
-	private CamelCaseSplitter ccs;
-	
-	@Before
-	public void init() {
-		ccs = new CamelCaseSplitter();
-	}
 	
 	@Test
 	public void testStringSemMaiuscula() {
 		List<String> strList = Arrays.asList("fulano");
-		assertEquals(strList, ccs.converterCamelCase("fulano")); 		
+		assertEquals(strList, CamelCaseSplitter.converterCamelCase("fulano")); 		
 	}
 }
