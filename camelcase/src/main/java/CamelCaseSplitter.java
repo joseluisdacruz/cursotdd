1c4a3e696748e5aed9e7f0614b43027ee4e6064d
joseluis <joseluisdacruz@gmail.com>
2019-04-14 19:33:49 -0300

    Refatoração

--- camelcase/src/main/java/CamelCaseSplitter.java
+++ camelcase/src/main/java/CamelCaseSplitter.java
@@ -9,16 +9,12 @@ public class CamelCaseSplitter {
 		if (isInicioNumerico(string) || hasCaractereInvalido(string)) {
 			throw new InvalidStringException("String inválido.");
 		}
-		List<String> retorno = Arrays.asList(string.split("(?=[A-Z0-9])"));
+		List<String> retorno = fatiar(string);
 		retorno = retorno.stream().reduce(
 				new ArrayList<String>(),
 				CamelCaseSplitter::adicionarString,
 				CamelCaseSplitter::listaCombiner);
 		return retorno.stream().map(CamelCaseSplitter::toLowerCase).collect(Collectors.toList());
-	}
-	
-	private static String toLowerCase(String str) {
-		return str.matches("[A-Z]+") ? str : str.toLowerCase();
 	}
 
 	private static List<String> adicionarString(List<String> lista, String str) {
@@ -27,14 +23,30 @@ public class CamelCaseSplitter {
 			return lista;
 		}
 		String ultimo = lista.get(lista.size() - 1);
-		if (ultimo.matches("[A-Z]+") && str.matches("[A-Z]") ||
-			ultimo.matches("[0-9]+") && str.matches("[0-9]")) {
+		if (isSigla(ultimo) && isMaiusculo(str) || 
+			isNumero(ultimo) && isDigito(str)) {
 			ultimo += str;
 			lista.set(lista.size() - 1, ultimo);
 		} else {
 			lista.add(str);
 		}
 		return lista;
+	}
+	
+	private static List<String> fatiar(String string) {
+		return Arrays.asList(string.split("(?=[A-Z0-9])"));
+	}
+
+	private static boolean isDigito(String str) {
+		return str.matches("[0-9]");
+	}
+
+	private static boolean isNumero(String str) {
+		return str.matches("[0-9]+");
+	}
+
+	private static boolean isMaiusculo(String str) {
+		return str.matches("[A-Z]");
 	}
 
 	private static List<String> listaCombiner(List<String> lista1, List<String> lista2) {
@@ -48,5 +60,13 @@ public class CamelCaseSplitter {
 	
 	private static boolean hasCaractereInvalido(String str) {
 		return !str.matches("[a-zA-Z0-9]+");
+	}
+	
+	private static String toLowerCase(String str) {
+		return isSigla(str) ? str : str.toLowerCase();
+	}
+
+	private static boolean isSigla(String str) {
+		return str.matches("[A-Z]+");
 	}
 }
