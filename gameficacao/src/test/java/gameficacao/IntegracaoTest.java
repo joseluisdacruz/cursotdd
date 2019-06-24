5246c6bdde6c3655436036a7b14c41e5f84c8559
Jose Luis Bras Marques da Cruz <jose-luis.cruz@serpro.gov.br>
2019-06-24 14:13:43 -0300

    Teste de integração

--- gameficacao/src/test/java/gameficacao/IntegracaoTest.java
+++ gameficacao/src/test/java/gameficacao/IntegracaoTest.java
@@ -1,7 +1,12 @@
 package gameficacao;
 
+import static gameficacao.TipoPonto.CURTIDA;
 import static gameficacao.TipoPonto.ESTRELA;
+import static gameficacao.TipoPonto.MOEDA;
+import static gameficacao.TipoPonto.TOPICO;
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -42,5 +47,30 @@ public class IntegracaoTest {
 		this.placar.registrar(new PontosUsuario("guerra", ESTRELA, 10));
 		assertEquals(new HashSet<Pontos>(Arrays.asList(new Pontos(ESTRELA, 10))),
 				this.placar.retornarPontosUsuario("guerra"));
+	}
+	
+	@Test
+	public void retornarPontosUsuario() {
+		Pontos pontos1 = new Pontos(ESTRELA, 15);
+		Pontos pontos2 = new Pontos(CURTIDA, 10);
+		Pontos pontos3 = new Pontos(TOPICO, 10);
+		this.placar.registrar(new PontosUsuario("jose", ESTRELA, 15));
+		this.placar.registrar(new PontosUsuario("jose", CURTIDA, 10));
+		this.placar.registrar(new PontosUsuario("jose", TOPICO, 10));
+		this.placar.registrar(new PontosUsuario("joao", MOEDA, 10));
+		assertEquals(
+				new HashSet<Pontos>(Arrays.asList(pontos1, pontos2, pontos3)),
+				this.placar.retornarPontosUsuario("jose"));
+	}
+	
+	@Test
+	public void recuperarRank() {
+		RankItem[] rank = {new RankItem("pedro", 20), new RankItem("joao", 15), new RankItem("jose", 10)};
+		this.placar.registrar(new PontosUsuario("joao", MOEDA, 15));
+		this.placar.registrar(new PontosUsuario("jose", MOEDA, 10));
+		this.placar.registrar(new PontosUsuario("pedro", MOEDA, 20));
+		this.placar.registrar(new PontosUsuario("fulano", CURTIDA,5));
+		this.placar.registrar(new PontosUsuario("maria", ESTRELA, 30));
+		assertArrayEquals(rank, this.placar.rank(MOEDA));
 	}
 }
